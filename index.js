948d577477db4bfb155f44ab248df084dd6ac967
ZagZx <zagzcraft@gmail.com>
2024-11-15 21:57:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits, Events} = require('discord.js')
+require('dotenv').config()
+const TOKEN = process.env.TOKEN
+
+client = new Client({ intents: 
+[
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages
+] 
+})
+
+
+//Evento que ocorre apenas uma vez. 
+//Quando o bot está pronto, executa a função (), que é basicamente o lambda de python, uma função sem nome
+client.once(Events.ClientReady, () =>{ 
+    console.log(`Estou online com o nick de ${client.user.username}`)
+})
+
+
+client.login(TOKEN)
